cb7e58569b2ef69b26bb13b038b40f82ebd07e39
ptodev <paulin.todev@gmail.com>
2015-01-28 21:46:55 +0000

    more functions

--- data/plot.py
+++ data/plot.py
@@ -1,6 +1,10 @@
 import pylab
 
-list_of_files = [("cv.txt", "data"),("cv2.txt", "data2")]
+# list_of_files = [("cv.txt",  "16x16 | 40 mcs | 5000 samples"),
+#                  ("cv2.txt", "32x32 | 30 mcs | 5000 samples")]
+
+list_of_files = [("m.txt",  "16x16 | 300 initial mcs | 30 mcs | 100 samples"),
+                 ("m2.txt", "100x100 | 150 initial mcs | 30 mcs | 50 samples")]
 
 datalist = [ ( pylab.loadtxt(filename), label ) for filename, label in list_of_files ]
 
@@ -8,9 +12,10 @@ for data, label in datalist:
     pylab.plot( data[:,0], data[:,1], label=label )
 
 #pylab.legend()
-pylab.title("16x16 | 40 mcs | 5000 samples")
+# pylab.title("")
 pylab.xlabel("kT/J")
-pylab.ylabel("C_v/(N*k)")
+pylab.ylabel("Average Magnetization Per Spin")
+pylab.legend()
 pylab.grid();
 
 pylab.show()
--- sim/plot.py
+++ sim/plot.py
@@ -1,6 +1,10 @@
 import pylab
 
-list_of_files = [("data.txt", "data")]
+# list_of_files = [("data.txt", "100x100 | 100 mcs | 100 samples | beta = 1/4")]
+#                  ("data2.txt", "100x100 | 100 mcs | 100 samples | beta = 1/2.2")]
+
+list_of_files = [("data.txt",  "100x100 | 500 mcs | 250 samples | beta = 1/1.5"),
+                 ("data2.txt", "100x100 | 500 mcs | 250 samples | beta = 1/2.2")]
 
 datalist = [ ( pylab.loadtxt(filename), label ) for filename, label in list_of_files ]
 
@@ -8,9 +12,10 @@ for data, label in datalist:
     pylab.plot( data[:,0], data[:,1], label=label )
 
 # pylab.legend()
-pylab.title("100x100 | 100 mcs | 100 samples | beta = 1/3")
+# pylab.title("")
+pylab.legend(loc='center')
 pylab.xlabel("mcs")
-pylab.ylabel("E*beta")
+pylab.ylabel("betaJ * E")
 pylab.grid();
 
 pylab.show()
